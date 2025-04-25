65902b4693b227daa1869d6d71efd87e7cd4ce84
Cédric Babeix <cedric.babeix@msbytes.com>
2025-04-25 16:14:45 +0200

    Update navbar.js

--- src/data/navbar.js
+++ src/data/navbar.js
@@ -4,8 +4,19 @@ module.exports = [
         path: '/en/',
         nav: [
             { href: '/', title: 'Home' },
-            { href: '/solution/', title: 'Solution' },
-            { href: '/product/data-model', title: 'Product' },
+            { href: '/solution/', title: 'Solution', subNav: [
+                { href: '/solution/itfm', title: 'A more strategic IT financial governance' },
+                { href: '/solution/tco', title: 'Total Cost of Ownership (TCO)' },
+                { href: '/solution/how-kp-one-helps-you', title: 'How KP-One helps you' },
+            ] },
+            { href: '/product/', title: 'Product', subNav: [
+                { href: '/product/data-model', title: 'An unprecedented data model' },
+                { href: '/product/standard-repository', title: 'A standard repository' },
+                { href: '/product/why-kp-one', title: 'Why choose KP-One?' },
+                { href: '/product/productivity-and-value-benefits', title: 'Productivity and value benefits' },
+                { href: '/product/features', title: 'KP-One features' },
+                { href: '/product/when-to-start', title: 'When to start with KP-One?' },
+            ] },
             { href: '/about/', title: 'About us' },
             { href: '/contact/', title: 'Contact' },
         ],
@@ -16,8 +27,19 @@ module.exports = [
         path: '/',
         nav: [
             { href: '/', title: 'Accueil' },
-            { href: '/solution/', title: 'Solution' },
-            { href: '/product/data-model', title: 'Produit' },
+            { href: '/solution/', title: 'Solution', subNav: [
+                { href: '/solution/itfm', title: 'Une gouvernance financière de l’informatique plus stratégique' },
+                { href: '/solution/tco', title: 'Le coût de détention ou TCO' },
+                { href: '/solution/how-kp-one-helps-you', title: 'Comment KP-One vous aide' },
+            ] },
+            { href: '/product/', title: 'Product', subNav: [
+                { href: '/product/data-model', title: 'Un modèle de données unique' },
+                { href: '/product/standard-repository', title: 'Un référentiel standard' },
+                { href: '/product/why-kp-one', title: 'Pourquoi choisir KP-One ?' },
+                { href: '/product/productivity-and-value-benefits', title: 'Vos gains de productivité et de valeur' },
+                { href: '/product/features', title: 'Fonctionnalités de KP-One' },
+                { href: '/product/when-to-start', title: 'Quand démarrer avec KP-One ?' },
+            ] },
             { href: '/about/', title: 'Qui sommes-nous ?' },
             { href: '/contact/', title: 'Contact' },
         ],
